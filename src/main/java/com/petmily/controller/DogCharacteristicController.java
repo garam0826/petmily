dbbe2274fa4e7e8978ae84344d8fff2379f72476
garam0826 <garam0826@naver.com>
2024-03-31 17:38:28 +0900

    실제 유기견별 특성 수치 예상

---
+++ src/main/java/com/petmily/controller/DogCharacteristicController.java
@@ -0,0 +1,32 @@
+package com.petmily.controller;
+
+import com.petmily.dto.ImageAnalysisResult;
+import com.petmily.dto.DogCharacteristics;
+import com.petmily.service.BreedCharacteristicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/dog-characteristics")
+public class DogCharacteristicController {
+
+    private final BreedCharacteristicService breedCharacteristicService;
+
+    @Autowired
+    public DogCharacteristicController(BreedCharacteristicService breedCharacteristicService) {
+        this.breedCharacteristicService = breedCharacteristicService;
+    }
+
+    @PostMapping("/calculate")
+    public ResponseEntity<DogCharacteristics> calculateCharacteristics(@RequestBody ImageAnalysisResult analysisResult) {
+        try {
+            DogCharacteristics dogCharacteristics = breedCharacteristicService.calculateAndSaveCharacteristics(analysisResult);
+            return ResponseEntity.ok(dogCharacteristics);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().build();
+        }
+    }
+}
+
