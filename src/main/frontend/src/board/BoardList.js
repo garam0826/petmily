e27c694adb67acf011ccd985fff5e46a88775fa3
lim678 <kyounglimb0@gmail.com>
2024-05-14 23:48:06 +0900

    게시판 UI 수정, 찜 기능 추가

--- src/main/frontend/src/board/BoardList.js
+++ src/main/frontend/src/board/BoardList.js
@@ -1,13 +1,11 @@
-//게시판 리스트 컨트롤러
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import {Link, useNavigate, useParams} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import './board.css';
 
 function BoardList() {
     const [boards, setBoards] = useState([]);
     const navigate = useNavigate();
-    //const { idx } = useParams();
 
     useEffect(() => {
         // API 호출
@@ -45,24 +43,6 @@ function BoardList() {
             </ul>
         </div>
     );
-    /*return (
-        <div>
-            <h2>게시글 목록</h2>
-            <ul>
-                {boards.map(board => (
-                    <li key={board.idx}>
-                        <Link to={`/board/list/read/${board.idx}`}>{board.title}</Link>- {board && board.reg_name} ({board && board.w_date})
-                    </li>
-                ))}
-            </ul>
-            <div>
-                <button onClick={() =>
-                    navigate("/board/list/write")}>
-                    글 작성하기
-                </button>
-            </div>
-        </div>
-    );*/
 }
 
 export default BoardList;
