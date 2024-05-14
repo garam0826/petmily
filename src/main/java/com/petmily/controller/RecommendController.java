7c8a55b17d9a8e44551edc23a0dc8e441de303f8
epris <epris_best@naver.com>
2024-05-14 23:35:33 +0900

    05월 14일

--- src/main/java/com/petmily/controller/RecommendController.java
+++ src/main/java/com/petmily/controller/RecommendController.java
@@ -39,7 +39,20 @@ public class RecommendController {
     // Logging
     private static final Logger logger = LoggerFactory.getLogger(RecommendController.class);
 
+    // 추천기능 1 : 강아지 품종 특성값 & 사용자 Profile 조합
+
+
+    // 추천기능 2 : 강아지 성격 Keyword 활용 -> 사용자 선택값과 강아지 품종의 일치 갯수
+    // 알고리즘 : UI에서 사용자가 선택 -> 선택한  csv파일 품종에서 keyword로 저장 ->
+    // 예시) 똑똑함(영단어 4개), 장난기 많은(영단어 2개), 민첩한(영단어 2개)
+
+
+
+
+    // 추천기능 3 : 협업 Filtering
     //
+
+
 
     // test
     @GetMapping("/test")
