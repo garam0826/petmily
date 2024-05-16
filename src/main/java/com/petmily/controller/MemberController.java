b0b560a60758cc804f9b1304a1a8c0117ed28e89
hjpark0417 <duffy35@naver.com>
2024-05-17 01:49:59 +0900

    Revert "PostMapping > GetMapping (시군 주소 문제 해결)"
    This reverts commit b88105f8fb13dbb88208b70a1d5311a8b1217a82.

--- src/main/java/com/petmily/controller/MemberController.java
+++ src/main/java/com/petmily/controller/MemberController.java
@@ -96,7 +96,7 @@ public class MemberController {
     // 시/군/구 주소 검색(광역 주소 기준)
     @GetMapping("/SearchDistrict")
     public ResponseEntity<List<DistrictDTO>> searchDistrict(@RequestParam("reg_name") String reg_name){
-        logger.info("/SearchDistrict GetMapping");
+        logger.info("/SearchDistrict PostMapping");
 
         try{
             List<DistrictDTO> d_List = memberService.searchDistrict(reg_name);
