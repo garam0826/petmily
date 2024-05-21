b5e42560be77cce6a2090db64957f580fa38b8e5
hjpark0417 <duffy35@naver.com>
2024-05-22 02:28:45 +0900

    버튼 css 수정

--- src/main/frontend/src/animal/AnimalInfo.jsx
+++ src/main/frontend/src/animal/AnimalInfo.jsx
@@ -199,9 +199,9 @@ const AnimalInfo = () => {
                     <div>
                         <h2>(현재 찜 기능으로 인해) 해당 화면은 로그인이 필요한 화면입니다.</h2>
                         <div style={{display: 'flex', justifyContent: 'center'}}>
-                            <button className="signup custom-button" onClick={() => navigate("/member/SignUp")}>회원가입 >
+                            <button className="custom-button" onClick={() => navigate("/member/SignUp")}>회원가입 >
                             </button>
-                            <button className="login custom-button" onClick={() => navigate("/member/login")}>로그인 >
+                            <button className="custom-button" onClick={() => navigate("/member/login")}>로그인 >
                             </button>
                         </div>
                     </div>
